7e965db371795e9b57c59da1209fe8ea81a05355
mwihoti <danielmwihoti@gmail.com>
2024-04-17 09:54:30 -0700

    change to SQLAlchemy

--- models/base_model.py
+++ models/base_model.py
@@ -42,8 +42,9 @@ class BaseModel:
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+
+        return '[{:s}] ({:s}) {}'.format(self.__class__.__name__, self.id,
+                                         self.__dict__)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
