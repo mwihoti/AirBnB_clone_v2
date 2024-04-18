31b7d61c52671525acfa71ae46971830cad086f7
mwihoti <danielmwihoti@gmail.com>
2024-04-17 18:53:51 -0700

    city.py correction

--- models/city.py
+++ models/city.py
@@ -6,12 +6,14 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
 
+
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = "cities"
         name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey("states.id", ondelete='CASCADE'), nullable=False)
+        state_id = Column(String(60),
+                          ForeignKey("states.id"), nullable=False)
         places = relationship("Place",
                               backref="cities",
                               cascade="all, delete-orphan")
--- models/place.py
+++ models/place.py
@@ -36,10 +36,10 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer,
                                 default=0,
                                 nullable=False)
-        #latitude = Column(Float, nullable=True)
-        #longitude = Column(Float, nullable=True)
-        #reviews = relationship("Review", cascade="all, delete",
-                              # backref="places")
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
+        reviews = relationship("Review", cascade="all, delete",
+                               backref="places")
         #amenities = relationship("Amenity",
                                  #secondary='place_amenity',
                                  #viewonly=False,
