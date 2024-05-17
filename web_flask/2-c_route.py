3898a8c65b98e6204a16e07701ba1410dbc7349a
mwihoti <danielmwihoti@gmail.com>
2024-05-17 16:23:56 +0300

    script that starts a Flask web application

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -9,16 +9,19 @@ app.url_map.strict_slashes = False
 
 @app.route('/')
 def hello():
+    """Defines Hello function"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def hbnb():
+    """Defines hbnb function"""
     return 'HBNB'
 
 @app.route('/c/<text>')
-def text():
-    return 'C'
+def c_text(text):
+    """defines text function for url parameter"""
+    return 'C {} '.format(text.replace('_', ' '))
 
 if __name__ == '__main__':
     app.run(debug=True)
