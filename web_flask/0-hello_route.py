ccf11186cf3de681609b3407b1adfbaf044c71a3
mwihoti <danielmwihoti@gmail.com>
2024-05-17 15:38:06 +0300

    Pycodestyle for 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,10 +9,10 @@ app.url_map.strict_slashes = False
 
 
 @app.route('/')
-
 def hello():
-	"""Prints Hello HBNB"""
-	return 'Hello HBNB!'
+    """Prints Hello HBNB"""
+    return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
