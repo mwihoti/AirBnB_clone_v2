6791218ef49f1a98ccc14a89786a5d5b97604133
mwihoti <danielmwihoti@gmail.com>
2024-05-17 19:29:10 +0300

    n is a number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""script that starts Flask"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """Defines Hello function"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Defines hbnb function"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """defines c_text function for url parameter"""
+    return 'C {} '.format(text.replace('_', ' '))
+
+
+@app.route('/python', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def py_text(text):
+    """defines py_text function for url parameter"""
+
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def n_num(n):
+    """Define n_num function"""
+    return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
