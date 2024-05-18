324a8ed49b2c31634f2408a8d04e855fd956f38e
mwihoti <danielmwihoti@gmail.com>
2024-05-18 19:35:04 +0300

    9. Cities by states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+
+from flask import Flask, render_template
+from models import storage
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def tear_storage(exception=None):
+    """removes the current SQLAlchemy Session
+    """
+    if storage is not None:
+        storage.close()
+
+
+@app.route('/cities_by_states')
+def cities_list(n=None):
+    """displays a HTML page: inside the tag BODY"""
+    # check 7-states_list.py and html for another way to do this
+    states = storage.all('State').values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
