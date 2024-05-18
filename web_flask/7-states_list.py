d6c5beeca75605055371b436b71496e604c2cdcc
mwihoti <danielmwihoti@gmail.com>
2024-05-18 16:59:21 +0300

    7-state_list of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+
+from flask import Flask, render_template
+from models import storage
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def tear_storage(exception=None):
+    """removes the current SQLAlchemy Session
+    """
+    if storage is not None:
+        storage.close()
+
+
+@app.route('/states_list')
+def states_list(n=None):
+    """display a HTML page: (inside the tag BODY"""
+    states = storage.all('State')
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
