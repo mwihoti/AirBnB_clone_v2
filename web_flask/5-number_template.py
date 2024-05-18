3905ae424b42e7a74e5132b86eb0db79dddfeeaa
mwihoti <danielmwihoti@gmail.com>
2024-05-18 10:03:21 +0300

    Displays html page if n is an integer

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""script that starts Flask"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """Defines Hello function"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Defines hbnb function"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """defines c_text function for url parameter"""
+    return 'C {} '.format(text.replace('_', ' '))
+
+
+@app.route('/python', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def py_text(text):
+    """defines py_text function for url parameter"""
+
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def n_num(n):
+    """Define n_num function"""
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<n>')
+def template(n):
+    """Define template function"""
+    return render_template('5-number.html', num=n)
+
+if __name__ == '__main__':
+    app.run(debug=True)
